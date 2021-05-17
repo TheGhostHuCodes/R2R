7ac7f802e7e8960a426f647fd10ecf2ae4966a77
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2021-05-17 11:21:11 -0700

    Return constant integer from Rust library

--- calculate/src/lib.rs
+++ calculate/src/lib.rs
@@ -2,5 +2,12 @@ use libc::{c_char, c_int};
 
 #[no_mangle]
 pub extern "C" fn solve(line: *const c_char, solution: *mut c_int) -> c_int {
+    if solution.is_null() {
+        return 1;
+    }
+    unsafe {
+        *solution = 1024;
+    }
+
     0
 }
