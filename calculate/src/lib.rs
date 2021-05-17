6e9a898593aa83b8f562135b2a6562058278fe99
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2021-05-17 12:38:10 -0700

    Validate C string before moving it into Rust

--- calculate/src/lib.rs
+++ calculate/src/lib.rs
@@ -1,10 +1,24 @@
 use libc::{c_char, c_int};
+use std::ffi::CStr;
 
 #[no_mangle]
 pub extern "C" fn solve(line: *const c_char, solution: *mut c_int) -> c_int {
     if solution.is_null() {
         return 1;
     }
+
+    let c_str = unsafe { CStr::from_ptr(line) };
+    let r_str = match c_str.to_str() {
+        Ok(s) => s,
+        Err(e) => {
+            eprintln!("UTF-8 Error: {}", e);
+            return 1;
+        }
+    };
+
+    println!("r_str.as_ptr(): {:p}, line: {:p}", r_str.as_ptr(), line);
+    println!("line: {}", r_str);
+
     unsafe {
         *solution = 1024;
     }
